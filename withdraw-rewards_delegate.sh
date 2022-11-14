0a40fe6dc5caabbc5f4c45a0a331a791a5368aef
ToTheMars2 <109024799+ToTheMars2@users.noreply.github.com>
2022-11-14 21:54:52 +0200

    Update withdraw-rewards_delegate.sh

--- withdraw-rewards_delegate.sh
+++ withdraw-rewards_delegate.sh
@@ -2,7 +2,6 @@
 bin=$1
 #time=$2
 name_wallet=$2
-to_val_address=$3
 file=$4
 chain=$5
 node=$6
@@ -13,9 +12,6 @@ pass=$7
 #  do
 #    (( from_count++ ))
   
-ADDR=$(echo $pass | $bin keys show $name_wallet --bech val --keyring-backend $file | grep address| sed "s/  address: //")
-
-echo "Val Address $ADDR"
 echo "Get comissi"
 
 echo $pass | $bin tx distribution withdraw-rewards $ADDR --from $name_wallet --commission --chain-id $chain --node $node -y
@@ -24,6 +20,8 @@ ADDR=$(echo $pass | $bin keys show $name_wallet --keyring-backend $file | grep a
 echo "Show balances"
 $bin q bank balances $ADDR
 
+ADDR=$(echo $pass | $bin keys show $name_wallet --bech val --keyring-backend $file | grep address| sed "s/  address: //")
 
+echo "Val Address $ADDR"
   
 done
