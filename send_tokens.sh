31c98bd9a1b2bb24af466b259bf0a38f0ecf303b
ToTheMars2 <109024799+ToTheMars2@users.noreply.github.com>
2022-11-30 18:01:19 +0200

    Update send_tokens.sh

--- send_tokens.sh
+++ send_tokens.sh
@@ -10,7 +10,7 @@ while [ $from_count -lt $nomber ]
   do
     (( from_count++ ))
 
-  ADDR=$(echo $pass | $bin keys show $from_count | grep address| sed "s/- address: //")
+  ADDR=$(echo $pass | $bin keys show $from_count | grep address| sed "s/^.*: //")
   echo "wallet $from_count $ADDR"
   echo $pass | $bin tx bank send $ADDR $to_address 10000000utlore --from $from_count -y
   
