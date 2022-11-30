8c576008dcbb8996043253df5d63e2cb1c000c6f
ToTheMars2 <109024799+ToTheMars2@users.noreply.github.com>
2022-11-30 18:00:54 +0200

    Update get_tokens_url.sh

--- get_tokens_url.sh
+++ get_tokens_url.sh
@@ -4,7 +4,7 @@ while [ $count -lt 1000 ]
   do
     (( count++ ))
   echo "\nwallet $count"
-  ADDR=$(gitopiad keys add $count < pass | grep address | sed  's/- address: //')
+  ADDR=$(gitopiad keys add $count < pass | grep address | sed  's/^.*: //')
 #  curl -X POST -d '{"address": "'"$ADDR"'", "coins": ["10000000unibi","100000000000unusd"]}' https://faucet.testnet-1.nibiru.fi/
   curl -s -X POST -d '{"address": "'"$ADDR"'"}' 'https://faucet.gitopia.com'
 
