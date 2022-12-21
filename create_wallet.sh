82a6f73c3874c21e2ace06e630bf7c791ff432bd
ToTheMars2 <109024799+ToTheMars2@users.noreply.github.com>
2022-12-21 21:47:56 +0200

    Update create_wallet.sh

--- create_wallet.sh
+++ create_wallet.sh
@@ -2,10 +2,11 @@
 count=0
 bin=$1
 nomber=${2:-"10"}
+pass="****"
 while [ $count -lt 15 ]
   do
     (( count++ ))
   echo -e "wallet $count\n"
-  ADDR=$(echo -e 'Papsan123\ny' | gitopiad keys add $count | grep address | sed  's/^.*: //')
+  ADDR=$(echo -e "$pass\ny"| $bin keys add $count | grep address | sed  's/^.*: //')
   
   done
