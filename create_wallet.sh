021179b30a2244dfa773354c9b9cba16d63791e6
ToTheMars2 <109024799+ToTheMars2@users.noreply.github.com>
2022-12-21 22:16:27 +0200

    Update create_wallet.sh

--- create_wallet.sh
+++ create_wallet.sh
@@ -3,10 +3,12 @@ count=0
 bin=$1
 nomber=${2:-"10"}
 pass="****"
+
+save="{}"
 while [ $count -lt 15 ]
   do
     (( count++ ))
   echo -e "wallet $count\n"
   ADDR=$(echo -e "$pass\ny"| $bin keys add $count | grep address | sed  's/^.*: //')
-  
+  echo "$save" | jq ".$count = $ADDR"
   done
