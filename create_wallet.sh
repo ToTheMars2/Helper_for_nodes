c5c21a737b7cdd0203dd0cd976e7114ab81f7c21
ToTheMars2 <109024799+ToTheMars2@users.noreply.github.com>
2022-12-22 02:33:14 +0200

    Update create_wallet.sh

--- create_wallet.sh
+++ create_wallet.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
-count=0
+count=1
 bin=$1
-nomber=${2:-"10"}
-pass="****"
+create_or_delete=${2:-"true"}
+nomber=${3:-"10"}
+pass="aaaa0005"
 
 save="{}"
-while [ $count -lt 15 ]
-  do
-    (( count++ ))
-  echo -e "wallet $count\n"
-  ADDR=$(echo -e "$pass\ny"| $bin keys add $count | grep address | sed  's/^.*: //')
-  echo "$save" | jq ".$count = $ADDR"
-done
-for i in $(echo "$save" | jq '. | keys | .[] | sed 's/\"//; s/\"$//' ' )
-do 
+if $create_or_delete
+then
+	while [ $count -le $nomber ]
+	  do
+	  echo -e "create wallet $count\n"
+	  ADDR=$(echo -e "$pass\ny"| $bin keys add $count | grep address | sed  's/^.*: //')
+	  c=\"$count\"
+          a=\"$ADDR\"
+          save=$(echo "$save" | jq ".$c = $a")
+	  echo "$save"
+          ((count++))
+          done
+	count=1
+        while [ $count -le $nomber ]
+	    do
+              c=\"$count\"
+	      value=$(echo "$save" | jq ".$c" | sed 's/\"//; s/\"$//' )
+	      echo "$count. $value"
+	      ((count++))
+             done
+else
+	while [ $count -le $nomber ]
+          do
+          echo -e "delete wallet $count\n"
+          ADDR=$(echo -e "$pass"| $bin keys delete $count -y)
+        ((count++))
+        done
+fi
 
-done
