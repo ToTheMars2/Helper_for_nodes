46be9f0819c6b2983173e123cb28391efa3094a3
ToTheMars2 <109024799+ToTheMars2@users.noreply.github.com>
2022-12-26 19:47:09 +0200

    Update vote.sh

--- vote/vote.sh
+++ vote/vote.sh
@@ -6,7 +6,7 @@ do
     platform=$(echo "$platforms" | sed  's/\"//; s/\"$//')
     bin=$(echo $list_gov_network | jq ".$platform.bin" | sed  's/\"//; s/\"$//')
     echo "$platform $bin $platforms"
-    for id in  $( echo "$(echo $list_gov_network | jq ".$platform.pass" | sed  's/\"//; s/\"$//' )"| $bin q gov proposals --reverse -o json | jq ' .proposals[] | select(.status=="PROPOSAL_STATUS_VOTING_PERIOD") |  .proposal_id')
+    for id in  $( echo "$(echo $list_gov_network | jq ".$platform.pass" | sed  's/\"//; s/\"$//' )"| $bin q gov proposals $(echo $list_gov_network | jq ".$platform.node" | sed  's/\"//; s/\"$//') --reverse -o json | jq ' .proposals[] | select(.status=="PROPOSAL_STATUS_VOTING_PERIOD") |  .proposal_id')
         do
           id=$(echo "$id"| sed  's/\"//; s/\"$//') # $(starsd q gov proposals --limit 11 --reverse -o json | jq ' .proposals[] | select(.status=="PROPOSAL_STATUS_VOTING_PERIOD") | .proposal_id')
           echo " $count. $id"
@@ -14,8 +14,8 @@ do
           if $(python3 check_json.py $platform $id )
           then
               date 
-              echo "$(echo $list_gov_network | jq ".$platform.bin" | sed  's/\"//; s/\"$//') tx gov vote $id yes $(echo $list_gov_network | jq ".$platform.from" | sed  's/\"//; s/\"$//') $(echo $list_gov_network | jq ".$platform.fees" | sed  's/\"//; s/\"$//') $(echo $list_gov_network | jq ".$platform.chain_id" | sed  's/\"//; s/\"$//')"
-              echo "$(echo $list_gov_network | jq ".$platform.pass" | sed  's/\"//; s/\"$//' )" | $(echo $list_gov_network | jq ".$platform.bin" | sed 's/\"//; s/\"$//') tx gov vote $id yes $(echo $list_gov_network | jq ".$platform.from" | sed  's/\"//; s/\"$//') $(echo $list_gov_network | jq ".$platform.fees" | sed  's/\"//; s/\"$//') $(echo $list_gov_network | jq ".$platform.chain_id" | sed  's/\"//; s/\"$//') -y
+              echo "$(echo $list_gov_network | jq ".$platform.bin" | sed  's/\"//; s/\"$//') tx gov vote $id yes $(echo $list_gov_network | jq ".$platform.from" | sed  's/\"//; s/\"$//') $(echo $list_gov_network | jq ".$platform.fees" | sed  's/\"//; s/\"$//') $(echo $list_gov_network | jq ".$platform.node" | sed  's/\"//; s/\"$//') $(echo $list_gov_network | jq ".$platform.chain_id" | sed  's/\"//; s/\"$//')"
+              echo "$(echo $list_gov_network | jq ".$platform.pass" | sed  's/\"//; s/\"$//' )" | $(echo $list_gov_network | jq ".$platform.bin" | sed 's/\"//; s/\"$//') tx gov vote $id yes $(echo $list_gov_network | jq ".$platform.from" | sed  's/\"//; s/\"$//') $(echo $list_gov_network | jq ".$platform.node" | sed  's/\"//; s/\"$//') $(echo $list_gov_network | jq ".$platform.fees" | sed  's/\"//; s/\"$//') $(echo $list_gov_network | jq ".$platform.chain_id" | sed  's/\"//; s/\"$//') -y
               echo 'TRUE'
           else
               echo "I voted for this proposol $platform $id "
