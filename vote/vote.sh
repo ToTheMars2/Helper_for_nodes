20ad7b1cb6a0cb1cf4445f14cd6e7bdc8e65d1b7
ToTheMars2 <109024799+ToTheMars2@users.noreply.github.com>
2022-12-28 13:04:05 +0200

    Update vote.sh

--- vote/vote.sh
+++ vote/vote.sh
@@ -5,11 +5,11 @@ for platforms in $(echo $list_gov_network | jq '. | keys | .[]')
 do
     platform=$(echo "$platforms" | sed  's/\"//; s/\"$//')
     bin=$(echo $list_gov_network | jq ".$platform.bin" | sed  's/\"//; s/\"$//')
-    echo "$platform $bin $platforms"
+    echo -e "\n$platform $bin $platforms\n"
     for id in  $( echo "$(echo $list_gov_network | jq ".$platform.pass" | sed  's/\"//; s/\"$//' )"| $bin q gov proposals $(echo $list_gov_network | jq ".$platform.node" | sed  's/\"//; s/\"$//') --reverse -o json | jq ' .proposals[] | select(.status=="PROPOSAL_STATUS_VOTING_PERIOD") |  .proposal_id')
         do
           id=$(echo "$id"| sed  's/\"//; s/\"$//') # $(starsd q gov proposals --limit 11 --reverse -o json | jq ' .proposals[] | select(.status=="PROPOSAL_STATUS_VOTING_PERIOD") | .proposal_id')
-          echo " $count. $id"
+          echo -e "\n$id\n"
 
           if $(python3 check_json.py $platform $id )
           then
