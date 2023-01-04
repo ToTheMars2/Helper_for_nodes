6d4bf0e8ab6b9aa77d81d5a034364f391a228901
ToTheMars2 <109024799+ToTheMars2@users.noreply.github.com>
2023-01-05 00:04:54 +0200

    Update get_tokens_url.sh

--- get_tokens_url.sh
+++ get_tokens_url.sh
@@ -6,7 +6,7 @@ pass="****"
 while [ $count -lt 1000 ]
   do
     (( count++ ))
-  echo "\nwallet $count"
+  echo -e "\nwallet $count"
   ADDR=$(echo "$pass" | $bin keys add $count --keyring-backend $keyring  | grep address | sed  's/^.*: //')
 #  curl -X POST -d '{"address": "'"$ADDR"'", "coins": ["10000000unibi","100000000000unusd"]}' https://faucet.testnet-1.nibiru.fi/
 #  curl -s -X POST -d '{"address": "'"$ADDR"'"}' 'https://faucet.gitopia.com'
